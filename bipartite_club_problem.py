c9e91e93e2351204027f09df801fce93fb85551e
Dylan Giletto <dgiletto@udel.edu>
2023-04-20 20:10:32 -0400

    BFS algorithm

--- bipartite_club_problem.py
+++ bipartite_club_problem.py
@@ -1,5 +1,7 @@
 import matplotviz as viz
 import club_setup as cs
+import BFS
 
 if __name__ == "__main__":
     viz.draw_graph(cs.g, show_edge_weights=False)
+    print(BFS.is_bipartite(cs.g, "Rob"))
