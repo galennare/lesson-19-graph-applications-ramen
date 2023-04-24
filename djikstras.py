227eb32b3e23a22beeb9a2b6f1769ac685f3eea9
Daniel Yang <daniely@udel.edu>
2023-04-24 12:26:07 -0400

    added files

--- djikstras.py
+++ djikstras.py
@@ -3,4 +3,6 @@ import matplotlib.pyplot as plt
 
 # Daniel Yang
 # The Problem
-# The University of Mars has an important task for you. 
+# The University of Mars has an important task for you. Since Mars is a relatively new planet to colonize, we need to be efficient with land.
+# We are asking you to find the shortest pathing through our graph of the buildings so that we can efficiently reserve land to build our campus.
+# The 
