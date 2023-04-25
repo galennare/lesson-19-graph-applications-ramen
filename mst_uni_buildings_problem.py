1c6c89df649ba1ff7e4df00059206359fe4e55a5
Galen <gnare@udel.edu>
2023-04-25 10:04:12 -0400

    Fix MST to use nx function instead

--- mst_uni_buildings_problem.py
+++ mst_uni_buildings_problem.py
@@ -1,11 +1,14 @@
 import matplotviz as viz
 import buildings_setup as bs
-import kruskals
+# import kruskals
+import networkx as nx
 
 if __name__ == '__main__':
-    mst = kruskals.kruskals_algorithm(bs.g)
+    mst = nx.minimum_spanning_tree(bs.g, algorithm="kruskal")
+    # mst = kruskals.kruskals_algorithm(bs.g)
     bs.pp.pprint(mst)
     bs.g.clear_edges()
-    for edge in mst:
+    for edge in mst.edges(data=True):
+        print(edge)
         bs.g.add_edge(edge[0], edge[1], weight=edge[2]['weight'])
     viz.draw_graph(bs.g, show_edge_weights=True)
